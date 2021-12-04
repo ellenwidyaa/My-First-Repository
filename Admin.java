8ae047ee8d21485dacd4e4c187277579af3a17cd
elvarettarl <95073909+elvarettarl@users.noreply.github.com>
2021-12-04 11:21:28 +0700

    create class admin

---
+++ Admin.java
@@ -0,0 +1,20 @@
+class Admin {
+    
+    Admin() {
+    }
+    
+    String lihatDokter() {
+        for (int i = 0; i < listDokter.length; i++) {
+            System.out.println(listDokter[i]);
+        }
+        
+        return "";
+}
+    String lihatPasien() {
+        for (int i = 0; i < listPasien.length; i++) {
+            System.out.println(listPasien[i]);
+        }
+
+        return "";
+}
+}
