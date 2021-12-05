87b9d05ed99dff502ed434ee3bc87dd51ce0dcc5
ellenwidyaa <74695281+ellenwidyaa@users.noreply.github.com>
2021-12-05 14:03:34 +0700

    Update Dokter.java

--- Dokter.java
+++ Dokter.java
@@ -6,29 +6,21 @@
 package rumah.sakit.pkg2;
 import java.util.*;
 
-public class RumahSakit2 {
-    
-    
-    public static void main(String[] args) {
-    Dokter d = new Dokter();
-    d.inputData();
-    d.showDokter();
-}
-    }
-
 class Dokter {
-    Dokter[] arr = new Dokter[3];
+    public Dokter[] arr = new Dokter[3];
     int pointer = 0;
     Scanner sc = new Scanner(System.in);
     private String nama; 
     private String day; 
     private String jam; 
+    private String spesialis;
     
-    public Dokter(String nama, String day, String jam)
+    public Dokter(String nama, String day, String jam, String spesialis)
     {
         this.nama = nama;
         this.day = day;
         this.jam = jam;
+        this.spesialis=spesialis;
     }
     
     public Dokter() {
@@ -59,14 +51,24 @@ class Dokter {
         return jam;
     }
     
+    public void setSpesialis(String spesialis) {
+        this.spesialis= spesialis;
+    }
+    
+    public String getSpesialis () {
+        return spesialis;
+    }
+    
     public void inputData() {
         System.out.print("Masukkan nama Anda : ");
-        String nama = sc.nextLine();
+        nama = sc.nextLine();
         System.out.print("Masukkan hari kerja Anda : ");
-        String day = sc.nextLine();
+        day = sc.nextLine();
         System.out.print("Masukkan jam kerja Anda : ");
-        String jam = sc.nextLine();
-        Dokter dokter = new Dokter(nama, day, jam);
+        jam = sc.nextLine();
+        System.out.print("Masukkan spesialis Anda : ");
+        spesialis = sc.nextLine();
+        Dokter dokter = new Dokter(nama, day, jam, spesialis);
         arr[pointer] = dokter;
         pointer++;
     }
@@ -90,6 +92,7 @@ class Dokter {
             System.out.printf("Nama dokter %d \t\t: \t%s \n", i + 1, dokter.getNama());
             System.out.printf("Hari dokter praktik %d \t: \t%s \n", i + 1, dokter.getDay());
             System.out.printf("Jam dokter praktik %d \t: \t%s \n", i + 1, dokter.getJam());
+            System.out.printf("Spesialisasi dokter %d \t: \t%s \n", i + 1, dokter.getSpesialis());
             System.out.println("----------------------------------------------");
         }
     }
@@ -97,6 +100,36 @@ class Dokter {
     public void kontrolPasien () {
         
     }
-}
-
- 
+    
+    public void obatPasien() {
+        
+    }
+    
+    public void methodDokter() {
+        int pilih = 0;
+        do{
+            System.out.println("Apa yang ingin Anda lakukan?");
+            System.out.println("Pilihan : ");
+            System.out.println("1. Memasukkan data Anda");
+            System.out.println("2. Daftar Pasien");
+            System.out.println("3. Lihat list Obat");
+            System.out.println("4. Memasukkan jadwal kontrol pasien");
+            System.out.println("5. Melihat daftar obat yang diberikan kepada pasien");
+            System.out.println("6. Keluar");
+            System.out.print("Masukkan Pilihan Menu : ");
+            pilih = sc.nextInt();
+                    sc.nextLine();
+            
+            switch (pilih){
+                case 1: inputData(); break;
+                case 2: listPasien(); break;
+                case 3: showObat (); break;
+                case 4: kontrolPasien(); break;
+                case 5: obatPasien(); break;
+                case 6: break;
+                default: System.out.println("Pilihan menu tidak ada!\n");
+            }
+        }
+        while (pilih != 6);  
+    }
+    }
