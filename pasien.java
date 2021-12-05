c84c20372cce3fdd4ab3af9f414392aa35e38ac4
elvarettarl <95073909+elvarettarl@users.noreply.github.com>
2021-12-05 13:48:58 +0700

    add arraylist dokter 1 2 3

--- pasien.java
+++ pasien.java
@@ -1,6 +1,10 @@
 
 class pasien {
     ArrayList<String> listPasien = new ArrayList<>();
+    ArrayList<String> listPasienKeDokter1 = new ArrayList<>();
+    ArrayList<String> listPasienKeDokter2 = new ArrayList<>();
+    ArrayList<String> listPasienKeDokter3 = new ArrayList<>();
+	
     static Scanner in = new Scanner(System.in);
 
     //        input data pasien
@@ -29,6 +33,7 @@ class pasien {
   
     }
     public void DaftarObat(){}
+	
     //lihat ketersediaan dokter
     void lihatAdaDokter() {
         for (int i = 0; i < listDokter.length; i++) {
@@ -53,41 +58,84 @@ class pasien {
 
         while(jalan) {
             System.out.println("-----Daftar disini-----");
-            System.out.println("Nama      : " + nama);
-            listPasien.add("Nama      : " + nama);
-            System.out.println("Umur      : " + umur);
-            listPasien.add("Umur      : " + umur);
-            System.out.println("Pekerjaan : " + pekerjaan);
-            listPasien.add("Pekerjaan : " + pekerjaan);
-            System.out.println("Alamat    : " + alamat);
-            listPasien.add("Alamat    : " + alamat);
-            System.out.println("Keluhan   : " + keluhan);
-            listPasien.add("Keluhan   : " + keluhan);
             System.out.println("Daftar Dokter yang Tersedia dan Jadwalnya : " + "\n" + lihatAdaDokter());
             System.out.println("Pilih dokter (masukkan angka)   : " + pilihan);
             switch(pilihan) {
                 case 1:
-                    listPasien.add("Dokter      : xxx" + "\n" + "Spesialis      : xxx");
+                    listPasien.add("Dokter 1");
+                    System.out.println("Nama      : " + nama);
+                    listPasien.add("Nama      : " + nama);
+		    listPasienKeDokter1.add("Nama      : " + nama);
+                    System.out.println("Umur      : " + umur);
+                    listPasien.add("Umur      : " + umur);
+		    listPasienKeDokter1.add("Umur      : " + umur);
+                    System.out.println("Pekerjaan : " + pekerjaan);
+                    listPasien.add("Pekerjaan : " + pekerjaan);
+		    listPasienKeDokter1.add("Pekerjaan : " + pekerjaan);
+                    System.out.println("Alamat    : " + alamat);
+                    listPasien.add("Alamat    : " + alamat);
+		    listPasienKeDokter1.add("Alamat    : " + alamat);
+                    System.out.println("Keluhan   : " + keluhan);
+                    listPasien.add("Keluhan   : " + keluhan);
+		    listPasienKeDokter1.add("Keluhan   : " + keluhan);
+                    System.out.println("Jadwal   : " + jadwal);
+                    listPasien.add("Jadwal   : " + jadwal);
+		    listPasienKeDokter1.add("Jadwal   : " + jadwal);
                     break;
                 case 2:
-                    listPasien.add("Dokter      : xxx" + "\n" + "Spesialis      : xxx");
+                    listPasien.add("Dokter 2");
+                    System.out.println("Nama      : " + nama);
+                    listPasien.add("Nama      : " + nama);
+		    listPasienKeDokter2.add("Nama      : " + nama);
+                    System.out.println("Umur      : " + umur);
+                    listPasien.add("Umur      : " + umur);
+		    listPasienKeDokter2.add("Umur      : " + umur);
+                    System.out.println("Pekerjaan : " + pekerjaan);
+                    listPasien.add("Pekerjaan : " + pekerjaan);
+		    listPasienKeDokter2.add("Pekerjaan : " + pekerjaan);
+                    System.out.println("Alamat    : " + alamat);
+                    listPasien.add("Alamat    : " + alamat);
+		    listPasienKeDokter2.add("Alamat    : " + alamat);
+                    System.out.println("Keluhan   : " + keluhan);
+                    listPasien.add("Keluhan   : " + keluhan);
+		    listPasienKeDokter2.add("Keluhan   : " + keluhan);
+                    System.out.println("Jadwal   : " + jadwal);
+                    listPasien.add("Jadwal   : " + jadwal);
+		    listPasienKeDokter2.add("Jadwal   : " + jadwal);
                     break;
                 case 3:
-                    listPasien.add("Dokter      : xxx" + "\n" + "Spesialis      : xxx");
+                    listPasien.add("Dokter 3");
+                    System.out.println("Nama      : " + nama);
+                    listPasien.add("Nama      : " + nama);
+		    listPasienKeDokter3.add("Nama      : " + nama);
+                    System.out.println("Umur      : " + umur);
+                    listPasien.add("Umur      : " + umur);
+		    listPasienKeDokter3.add("Umur      : " + umur);
+                    System.out.println("Pekerjaan : " + pekerjaan);
+                    listPasien.add("Pekerjaan : " + pekerjaan);
+		    listPasienKeDokter3.add("Pekerjaan : " + pekerjaan);
+                    System.out.println("Alamat    : " + alamat);
+                    listPasien.add("Alamat    : " + alamat);
+		    listPasienKeDokter3.add("Alamat    : " + alamat);
+                    System.out.println("Keluhan   : " + keluhan);
+                    listPasien.add("Keluhan   : " + keluhan);
+		    listPasienKeDokter3.add("Keluhan   : " + keluhan);
+                    System.out.println("Jadwal   : " + jadwal);
+                    listPasien.add("Jadwal   : " + jadwal);
+		    listPasienKeDokter3.add("Jadwal   : " + jadwal);
                     break;
             }
-            System.out.println("Jadwal   : " + jadwal);
-            listPasien.add("Jadwal   : " + jadwal);
-
             System.out.println("Pendaftaran berhasil. Silakan beralih ke menu pembayaran.");
             jalan = false;
         }
     }
-       System.out.println("========DATA PASIEN========");
+	
+        System.out.println("========DATA PASIEN========");
         System.out.print("Nama              : ");
-    nama = in.nextLine();
+        nama = in.nextLine();
         System.out.print("Id Pasien         : ");
-    id_member = in.nextLine();
+        id_member = in.nextLine();
+	
 //        Pilihan Menu
         System.out.println("Pilihan Menu : ");
         System.out.println("1. Daftar");
@@ -98,7 +146,7 @@ class pasien {
         System.out.println("6. Status Ketersediaan Dokter");
 	System.out.println("7. Keluar");
         System.out.print("Masukkan Pilihan Menu : ");
-    String pilihan = in.nextLine();
+        String pilihan = in.nextLine();
             switch(pilihan) {
         case "1":
             daftarMandiri();
@@ -113,10 +161,13 @@ class pasien {
             BiayaPerawatan();
             break;
         case "5":
-            DaftarObat()break;
+            DaftarObat()
+	    break;
         case "6":
-            lihatAdaDokter();break;
-        case "7": break;
+            lihatAdaDokter();
+            break;
+        case "7":
+            break;
         default:
             System.out.println("Pilihan tidak ada");break;
     }
