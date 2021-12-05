9d9d7ac5b29470a683c515d1613de68d476f79b0
berliandwia <95073929+berliandwia@users.noreply.github.com>
2021-12-05 14:39:54 +0700

    Update pasien.java

--- pasien.java
+++ pasien.java
@@ -1,36 +1,116 @@
-
 class pasien {
-    ArrayList<String> listPasien = new ArrayList<>();
-    ArrayList<String> listPasienKeDokter1 = new ArrayList<>();
-    ArrayList<String> listPasienKeDokter2 = new ArrayList<>();
-    ArrayList<String> listPasienKeDokter3 = new ArrayList<>();
-	
     static Scanner in = new Scanner(System.in);
+    pasien[]arr= new pasien[100];
+    int pointer=0;
+    private String name;
+    private int age;
+    private String address;
+    private String gender;
+    private String keluhan;
+    private String Pekerjaan;
+    private int jawabDokter;
 
-    //        input data pasien
-    String nama, id_member, jenis_kelamin, umur, pekerjaan;
+    public int getAge() {
+        return age;
+    }
 
-    public void JadwalKontrol(){}
-    
-    public void JadwalDokter(){}
-    public void BiayaPerawatan(){
-       int harga1,harga2,harga3;
+    public int getJawabDokter() {
+        return jawabDokter;
+    }
 
-        switch(noDokter) {
-            case 1:
-                harga1 = 150000;
-                System.out.println("Biaya perawatan : " + harga1);
-                break;
-            case 2:
-                harga1 = 200000;
-                System.out.println("Biaya perawatan : " + harga2);
-                break;
-            case 3:
-                harga1 = 100000;
-                System.out.println("Biaya perawatan : " + harga3);
-                break;
-       }
-  
+    public void setJawabDokter(int jawabDokter) {
+        this.jawabDokter = jawabDokter;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getKeluhan() {
+        return keluhan;
+    }
+
+    public String getPekerjaan() {
+        return Pekerjaan;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        Pekerjaan = pekerjaan;
+    }
+
+    public void setKeluhan(String keluhan) {
+        this.keluhan = keluhan;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+ public void daftarMandiri() {
+        System.out.println("=======DAFTAR PASIEN============");
+        System.out.println("Nama            : ");
+        String name = in.nextLine();
+        System.out.println("Umur            : ");
+        int age = in.nextInt();
+        in.nextLine();
+        System.out.println("Alamat  :       : ");
+        String address = in.nextLine();
+        System.out.println("Jenis kelamin   : ");
+        String gender = in.nextLine();
+        System.out.println("Pekerjaan       : ");
+        String Pekerjaan = in.nextLine();
+        System.out.println("Keluhan          : ");
+        String keluhan = in.nextLine();
+        System.out.println("Silahkan pilih dokter yang tersedia ");
+//      panggil method list dokter
+        System.out.println("Jawaban : ");
+        int jawabDokter = in.nextInt();
+        System.out.println("Pendaftaran berhasil. Silakan melakukan konsultasi dengan dokter yang dipilih");
+        pasien Pasien = new pasien(name,age,address,gender,Pekerjaan,keluhan,jawabDokter);
+        arr[pointer]=pasien;
+        pointer++;
+        }
+
+
+    public void ListPasien() {
+        System.out.println(=========="LIST PASIEN===============");
+    for(int i = 0; i < arr.length;i++) {
+        pasien Pasien = arr[i];
+        if (Pasien == null) {
+            System.out.println("==================================================");
+            return;
+        }
+        System.out.printf("Nama Pasein %d \t\t: \t%s \n", i + 1, Pasien.getName());
+        System.out.printf("Umur Pasien %d \t\t: \t%s \n", i + 1, Pasien.getAge());
+        System.out.printf("Alamat Pasein %d \t\t: \t%s \n", i + 1, Pasien.getAddress());
+        System.out.printf("Jenis Kelamin Pasein %d \t\t: \t%s \n", i + 1, Pasien.getGender());
+        System.out.printf("Pekerjaan Pasein %d \t\t: \t%s \n", i + 1, Pasien.getPekerjaan());
+        System.out.printf("Keluhan Pasein %d \t\t: \t%s \n", i + 1, Pasien.getKeluhan());
+        System.out.printf("Dokter pilihan Pasein %d \t\t: \t%s \n", i + 1, Pasien.getJawabDokter());
+        System.out.println("----------------------------------------------");
+    }
     }
     public void DaftarObat(){}
 	
